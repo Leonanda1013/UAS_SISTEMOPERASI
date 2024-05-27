8a42236d4f54dd6698356ef31a6ce6a8ae7df39c
leonanda1013 <leonandaprabowo@gamil.com>
2024-05-27 22:11:45 +0700

    update

--- README.md
+++ README.md
@@ -7,3 +7,18 @@
 | Kelas | TI-1B |
 | Dosen Pengampu 1 | Erfan Rohadi, S.T., M.Eng., Ph.D |
 | Dosen Pengampu 2 | [Mohammad Faried Rahmat, S.ST., M.Tr.T](https://github.com/mrhmt80) |SDSDVSDV
+
+
+# SISTEM OPERASI
+### Fitur VArchiver
+|  |  |  |
+|--|--|--|
+|**No**| **Fitur** | **Detail** |
+| 1 | Lihat File | Melihat daftar isi sebuah direktori dengan perintah ***ls -l*** |
+| 2 | Hapus File/Folder | Menghapus file dari sebuah direktori dengan perintah ***rm -i*** dan menghapus sebuah folder dengan perintah ***rm -r***|
+| 3 | Copy File | Menyalin file dengan perintah ***cp*** |
+| 4 | Move File | Memeindahkan file dengan perintah ***mv*** |
+| 5 | Kompres File | Mengompres file dengan perintah ***gzip -c*** |
+| 6 | Ekstrak File | Mengekstrak file terkompres dengan perintah ***gunzip*** |
+| 7 | Kompres Folder | Mengompres folder dengan perintah ***tar -czf*** |
+| 8 | Ekstrak Folder | Mengekstrak folder dengan perintah ***tar -xf*** |
---
+++ tugasAkhir.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+show_menu(){
+    echo "Menu:"
+    echo "1. Buat file"
+    echo "2. Keluar"
+    echo "3. Lihat File"
+    echo "4. Hapus File/Folder"
+    echo "5. Copy File"
+    echo "6. Move File"
+    echo "7. Kompres File"
+    echo "8. Ekstrak File"
+    echo "9. Kompres Folder"
+    echo "10. Ekstrak Folder"
+}
+
+create_file(){
+    touch "$1"
+    echo "File $1 berhasil dibuat"
+}
+
+list_files(){
+    ls -l
+}
+
+delete_file_or_folder(){
+    read -p "Masukkan nama file/folder yang ingin dihapus: " file_or_folder
+    if [ -d "$file_or_folder" ]; then
+        rm -r "$file_or_folder"
+    else
+        rm -i "$file_or_folder"
+    fi
+    echo "$file_or_folder berhasil dihapus"
+}
+
+copy_file(){
+    read -p "Masukkan nama file yang ingin disalin: " src
+    read -p "Masukkan tujuan salinan: " dest
+    cp "$src" "$dest"
+    echo "File $src berhasil disalin ke $dest"
+}
+
+move_file(){
+    read -p "Masukkan nama file yang ingin dipindahkan: " src
+    read -p "Masukkan tujuan pemindahan: " dest
+    mv "$src" "$dest"
+    echo "File $src berhasil dipindahkan ke $dest"
+}
+
+compress_file(){
+    read -p "Masukkan nama file yang ingin dikompres: " file
+    gzip -c "$file" > "$file.gz"
+    echo "File $file berhasil dikompres menjadi $file.gz"
+}
+
+extract_file(){
+    read -p "Masukkan nama file terkompres (dengan ekstensi .gz) yang ingin diekstrak: " file
+    gunzip "$file"
+    echo "File $file berhasil diekstrak"
+}
+
+compress_folder(){
+    read -p "Masukkan nama folder yang ingin dikompres: " folder
+    tar -czf "$folder.tar.gz" "$folder"
+    echo "Folder $folder berhasil dikompres menjadi $folder.tar.gz"
+}
+
+extract_folder(){
+    read -p "Masukkan nama folder terkompres (dengan ekstensi .tar.gz) yang ingin diekstrak: " folder
+    tar -xf "$folder"
+    echo "Folder $folder berhasil diekstrak"
+}
+
+while true; do
+    show_menu
+    read -p "Masukkan pilihan: " choice
+    case $choice in
+        1)
+            read -p "Masukkan nama file: " nama_file
+            create_file "$nama_file"
+            ;;
+        2)
+            echo "Keluar dari program."
+            exit
+            ;;
+        3)
+            list_files
+            ;;
+        4)
+            delete_file_or_folder
+            ;;
+        5)
+            copy_file
+            ;;
+        6)
+            move_file
+            ;;
+        7)
+            compress_file
+            ;;
+        8)
+            extract_file
+            ;;
+        9)
+            compress_folder
+            ;;
+        10)
+            extract_folder
+            ;;
+        *)
+            echo "Pilihan tidak valid."
+            ;;
+    esac
+done
